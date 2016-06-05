50fab96e33a2f697310c626eeee94f36261740af
Benny Xie <benny-xie@hotmail.com>
2016-06-05 17:03:22 -0400

    Code cleanup: 	change comments to english

--- weiboapi/send_weibo.py
+++ weiboapi/send_weibo.py
@@ -9,15 +9,16 @@ from weibo import APIClient
 
 def get_access_token(app_key, app_secret, callback_url):
     client = APIClient(app_key=app_key, app_secret=app_secret, redirect_uri=callback_url)
-    # 获取授权页面网址
     auth_url = client.get_authorize_url()
     print auth_url
     
-    # 在浏览器中访问这个URL，会跳转到回调地址，回调地址后面跟着code，输入code
+    # Visit printed url in your web brouser, log in to your weibo account,
+    # After the page jumps to callback url, copy the code following the
+    # callback url.
+    # note: you have to use the weibo account that applies for the app.
     code = raw_input("Input code:")
     r = client.request_access_token(code)
     access_token = r.access_token
-    # token过期的UNIX时间
     expires_in = r.expires_in
     print 'access_token:',access_token
     print 'expires_in:', expires_in
@@ -29,8 +30,10 @@ if __name__ == '__main__':
     app_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxx'
     callback_url = 'https://api.weibo.com/oauth2/default.html'
 
+    # After running the command once, you could get the 'access_token' 
+    # and 'expires in'. Save them so that you don't need to you that 
+    # every time.
     access_token, expires_in = get_access_token(app_key, app_secret, callback_url)
-    # 上面的语句运行一次后，可保存得到的access token，不必每次都申请
     # access_token = 'xxxxx'
     # expires_in = 'xxxxx'
 
@@ -42,9 +45,9 @@ if __name__ == '__main__':
     default_msg_part_2 = ' msg sent automatically from benny"s robot HAHAHA'
  
     # send a weibo with img
-    f = open('test.jpg', 'rb')
+    f = open('../test.jpg', 'rb')
     r = client.statuses.upload.post(status=u'test: weibo with an img. -benny', pic=f)
-    f.close() # APIClient不会自动关闭文件，需要手动关闭
+    f.close()
 
     # send text weibo every 200sec
     while True:
@@ -52,5 +55,5 @@ if __name__ == '__main__':
 	utext = unicode(line,"UTF-8") 
         client.post.statuses__update(status=utext) 
 	idx = idx + 1
-        time.sleep(200)    
+        time.sleep(20)    
 
